928dc6437d1696ba3171aa1fdd31ccc62aa38116
Hayan Khan <hayankhanmehsood@gmail.com>
2024-07-23 21:44:01 +0500

    .

--- prac1.js
+++ prac1.js
@@ -5,20 +5,31 @@ const age=25;
 
 let newName = name.slice(9,3)
 let nName='   hayan is  here  '
-console.log(nName)
-console.log(nName.trim())
+// console.log(nName)
+// console.log(nName.trim())
 
 
 const url="https://www.google%20name.com";
-console.log(url.replace("%20","-"))
-console.log(url.includes("google"))
+// console.log(url.replace("%20","-"))
+// console.log(url.includes("google"))
 
 
 let str = "My-name-is*hayan"; 
-console.log(str.split("-"))
-console.log(str.concat(" I am a student"))
+// console.log(str.split("-"))
+// console.log(str.concat(" I am a student"))
 
 const num = 23.8966
 
-console.log(num.toFixed(2))
-console.log(num.toPrecision(3))
+// console.log(num.toFixed(2))
+// console.log(num.toPrecision(3))
+
+
+
+// MATHS
+
+let arr= [1,2,3,4,5,6,7,8,9,10]
+
+console.log(Math.PI.toFixed(2))
+console.log(Math.round(4.38))
+console.log(Math.ceil(4.38))
+console.log(Math.max(3,4,5,6))
