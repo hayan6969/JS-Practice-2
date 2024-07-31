841068bcc6d621dace11d41ef549c60d3329ca6e
Hayan Khan <hayankhanmehsood@gmail.com>
2024-07-31 22:55:23 +0500

    .

--- myClass.js
+++ myClass.js
@@ -8,6 +8,11 @@ class User{
         return `${this.name}${this.email}`;
 
     }
+
+    static createUserId(){  //now this method is not available to the instance of the class
+        return `${this.name}${this.email}`;
+
+    }
 }
 
 const chai = new User('Chai','daffa')
@@ -19,3 +24,22 @@ console.log(chai.encrypt()) // Output: Chai daffa
 
 //if it werent for class we would have to create first a function that takes parameters and returns an object
 //then add the method encrypt() or any other method to the prototype of the function
+
+
+
+//Inheritance
+
+class Admin extends User{
+    constructor(name,email){
+        super(name)
+        this.email = email;
+        console.log(this)
+    }
+
+}
+
+
+const admin = new Admin('Cha1','daffa')
+console.log(admin.encrypt()) // Output: Chai daffa
+
+
