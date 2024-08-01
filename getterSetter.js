9803a47e7b621c59da998008cdbef6096722a6eb
Hayan Khan <hayankhanmehsood@gmail.com>
2024-08-01 16:07:00 +0500

    ....

---
+++ getterSetter.js
@@ -0,0 +1,11 @@
+class User{
+    constructor(name,email){
+        this.name = name;
+        this.email = email;
+    }
+
+    
+}
+
+const user = new User("hayan","hayan@gmail.com")
+console.log(user.email)
