9ddbc3faa661b2e769365b61491c9fbc184d8c5b
Hayan Khan <hayankhanmehsood@gmail.com>
2024-08-01 16:33:46 +0500

    ...

--- getterSetter.js
+++ getterSetter.js
@@ -18,3 +18,17 @@ class User{
 const user = new User("hayan","hayan@gmail.com")
 user.email="hello"
 console.log(user.email)
+
+const func = ()=>{
+    const a = 123
+
+    const func2=()=>{
+        const func3=()=>{
+            console.log(a)
+        }
+        func3()
+    }
+    func2()
+}
+
+func()
